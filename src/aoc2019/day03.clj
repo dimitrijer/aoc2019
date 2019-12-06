24032aa4d072f493971150b342779205eafab227
Dimitrije Radojević <dimitrijer@nordeus.com>
2019-12-07 00:50:21 +0100

    Working day 03 puzzle 1 solution

--- src/aoc2019/day03.clj
+++ src/aoc2019/day03.clj
@@ -3,10 +3,17 @@
 
 
 (def paths 
-["R8,U5,L5,D3" "U7,R6,D4,L4"]
-#_["R75,D30,R83,U83,L12,D49,R71,U7,L72" "U62,R66,U55,R34,D71,R55,D58,R83"]
-#_["R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51" "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"]
-#_(str/split-lines (slurp "resources/day03_puzzle1.txt")))
+
+  #_["R8,U5,L5,D3"
+     "U7,R6,D4,L4"]
+
+  #_["R75,D30,R83,U83,L12,D49,R71,U7,L72"
+     "U62,R66,U55,R34,D71,R55,D58,R83"]
+
+  #_["R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51"
+     "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"]
+
+  (str/split-lines (slurp "resources/day03_puzzle1.txt")))
 
 
 ;; Parsing
@@ -57,11 +64,19 @@
     [invariant (sort variant)]))
 
 
+
+(defn orthogonal?
+  [lhs-segment rhs-segment]
+  (or (and (= (:dir lhs-segment) :v) (= (:dir rhs-segment) :h))
+      (and (= (:dir lhs-segment) :h) (= (:dir rhs-segment) :v))))
+
+
 (defn intersect?
   [lhs-segment rhs-segment]
   (let [[lhs-invariant lhs-variant] (varinvar lhs-segment)
         [rhs-invariant rhs-variant] (varinvar rhs-segment)]
-    (and (> lhs-invariant (first rhs-variant))
+    (and (orthogonal? lhs-segment rhs-segment)
+         (> lhs-invariant (first rhs-variant))
          (< lhs-invariant (second rhs-variant))
          (> rhs-invariant (first lhs-variant))
          (< rhs-invariant (second lhs-variant)))))
