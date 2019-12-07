35e2ba67cafd560d9b81a2991cc5c62f2756f06e
Dimitrije Radojević <dimitrijer@nordeus.com>
2019-12-07 01:29:45 +0100

    Implement puzzle 2 solution for day 03

--- src/aoc2019/day03.clj
+++ src/aoc2019/day03.clj
@@ -13,6 +13,12 @@
   #_["R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51"
      "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"]
 
+  #_["R75,D30,R83,U83,L12,D49,R71,U7,L72"
+     "U62,R66,U55,R34,D71,R55,D58,R83"]
+
+  #_["R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51"
+     "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"]
+
   (str/split-lines (slurp "resources/day03_puzzle1.txt")))
 
 
@@ -36,11 +42,15 @@
               \U [x (+ y length)]
               \D [x (- y length)])
 
-        next-segment {:dir (directions->dir direction)
-                      :start start
-                      :end end}]
+        prev-steps (or (:steps (last segments)) 0)
+        prev-length (or (:length (last segments) 0))
 
-    (conj segments next-segment)))
+        segment {:dir (directions->dir direction)
+                 :start start :end end
+                 :length length
+                 :steps (+ prev-steps prev-length)}]
+
+    (conj segments segment)))
 
 
 (defn path->wire [path] (reduce parse-segments [] (str/split path #",")))
@@ -87,9 +97,18 @@
   (for [red-segment red-wire
         blue-segment blue-wire
         :when (intersect? red-segment blue-segment)]
-    (if (= :v (:dir red-segment))
-      [(first (varinvar red-segment)) (first (varinvar blue-segment))]
-      [(first (varinvar blue-segment)) (first (varinvar red-segment))])))
+
+    (let [intersection (if (= :v (:dir red-segment))
+                         [(first (varinvar red-segment)) (first (varinvar blue-segment))]
+                         [(first (varinvar blue-segment)) (first (varinvar red-segment))])
+
+          steps (+ (:steps red-segment)
+                   (:steps blue-segment)
+                   (Math/abs (reduce + (mapv - intersection (:start red-segment))))
+                   (Math/abs (reduce + (mapv - intersection (:start blue-segment)))))]
+
+      {:point intersection
+       :steps steps})))
 
 
 (defn manhattan [[x y]] (+ (Math/abs x) (Math/abs y)))
@@ -98,4 +117,12 @@
 (defn puzzle1
   []
   (let [[red-wire blue-wire] (map path->wire paths)]
-    (apply min (map manhattan (get-intersections red-wire blue-wire)))))
+    (apply min (map (comp manhattan :point) (get-intersections red-wire blue-wire)))))
+
+
+(defn puzzle2
+  []
+  (let [[red-wire blue-wire] (map path->wire paths)]
+    (println red-wire)
+    (println blue-wire)
+    (apply min (map :steps (get-intersections red-wire blue-wire)))))
